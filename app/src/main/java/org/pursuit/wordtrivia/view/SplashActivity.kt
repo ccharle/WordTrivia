724ba1d8450eabc0737f2b27b7044a97ff4ff38f
ccharle <cliffordcharles@pursuit.org>
2019-10-18 02:32:04 -0400

    add sound fx

--- app/src/main/java/org/pursuit/wordtrivia/view/SplashActivity.kt
+++ app/src/main/java/org/pursuit/wordtrivia/view/SplashActivity.kt
@@ -1,15 +1,18 @@
 package org.pursuit.wordtrivia.view
 
 import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import org.pursuit.wordtrivia.R
+import org.pursuit.wordtrivia.audio.AudioLoader
 
 class SplashActivity : AppCompatActivity() {
 
     private var delayHandler: Handler? = null
-    private val splashDelay: Long = 5000
+    private val splashDelay: Long = 1500
+    private val audioLoader: AudioLoader = AudioLoader(this, MediaPlayer())
 
     private val runnable: Runnable = Runnable {
         if (!isFinishing) {
@@ -25,15 +28,15 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.splash_screen)
 
         delayHandler = Handler()
-
         delayHandler!!.postDelayed(runnable, splashDelay)
-
+        audioLoader.onSplashScreen()
     }
 
     public override fun onDestroy() {
 
         if (delayHandler != null) {
             delayHandler!!.removeCallbacks(runnable)
+            audioLoader.releaseMediaPlayer()
         }
 
         super.onDestroy()
